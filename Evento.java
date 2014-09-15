d3d70a329e38121880566fc3a4cfdc8be07e42a7
chivijr <chivijr@gmail.com>
2014-09-16 00:17:49 +0200

    Create Evento.java

---
+++ Evento.java
@@ -0,0 +1,25 @@
+package aplicacion;
+
+public class Evento {
+	String IdEvento;
+	String Titulo;
+	String Precio;
+	String Gratuito;
+	String LargaDuracion;
+	String DiasSemanas;
+	String DiasExcluido;
+	String Fecha;
+	String FechaFin;
+	String Hora;
+	String Descripcion;
+	String ContentUrl;
+	String TituloActividad;
+	String UrlActividad;
+	String UrlInstalacion;
+	String NombreInstalacion;
+	String CoordenadaX;
+	String CoordenadaY;
+	String CoordenadaLatitud;
+	String CoordenadaLongitud;
+	String Tipo;
+}
